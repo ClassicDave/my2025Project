8f398b5da781a42983678c041cd94bf223d5c829
ClassicDave <111157351+ClassicDave@users.noreply.github.com>
2025-03-24 17:20:09 +0200

    Add files via upload

--- Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/results/GameResultsService.kt
+++ Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/results/GameResultsService.kt
@@ -1,10 +1,42 @@
 package jetbrains.kotlin.course.alias.results
-
+import jetbrains.kotlin.course.alias.team.Team
+import jetbrains.kotlin.course.alias.team.TeamService
 import org.springframework.stereotype.Service
+
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
+import java.io.File
+
+@Serializable
+data class SerializableGameResult(val teams: List<SerializableTeam>)
+
+typealias GameResult = List<Team>
 
 @Service
 class GameResultsService {
-    fun saveGameResults(result: GameResult): Unit = TODO("Not implemented yet")
+    companion object {
+        val gameHistory: MutableList<GameResult> = mutableListOf()
+    }
 
-    fun getAllGameResults(): List<GameResult> = TODO("Not implemented yet")
+    fun saveGameResults(result: GameResult) {
+        require(result.isNotEmpty()) { "Result cannot be empty" }
+        require(result.all { it.id in TeamService.teamsStorage.keys }) { "Invalid team IDs" }
+        gameHistory.add(result)
+        saveGameHistory()
+    }
+
+    private fun saveGameHistory() {
+        val json = Json.encodeToString(gameHistory.map { SerializableGameResult(it.map { t -> SerializableTeam(t.id, t.points) }) })
+        File("gameHistory.json").writeText(json)
+    }
+
+    fun loadGameHistory() {
+        val file = File("gameHistory.json")
+        if (file.exists()) {
+            val history = Json.decodeFromString<List<SerializableGameResult>>(file.readText())
+            gameHistory.addAll(history.map { it.teams.map { t -> Team(t.id, t.points) } })
+        }
+    }
+
+    fun getAllGameResults(): List<GameResult> = gameHistory.reversed()
 }
