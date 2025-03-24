8a9f718c47c886f87921cc63fb6ad4c2400ee5cc
ClassicDave <111157351+ClassicDave@users.noreply.github.com>
2025-03-24 17:22:00 +0200

    Add files via upload

--- Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/team/TeamService.kt
+++ Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/team/TeamService.kt
@@ -1,9 +1,42 @@
 package jetbrains.kotlin.course.alias.team
 
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
+import java.io.File
+
+import jetbrains.kotlin.course.alias.util.Identifier
+import jetbrains.kotlin.course.alias.util.IdentifierFactory
 import org.springframework.stereotype.Service
+
+
+@Serializable
+data class SerializableTeam(val id: Identifier, val points: Int)
 
 @Service
 class TeamService {
+    private val identifierFactory = IdentifierFactory()
 
-    fun generateTeamsForOneRound(teamsNumber: Int): List<Team> = TODO("Not implemented yet")
+    companion object {
+        val teamsStorage: MutableMap<Identifier, Team> = mutableMapOf()
+    }
+
+    fun generateTeamsForOneRound(teamCount: Int): List<Team> {
+        val teams = List(teamCount) { Team(identifierFactory.uniqueIdentifier()) }
+        teams.forEach { teamsStorage[it.id] = it }
+        return teams
+    }
+    private fun saveTeams() {
+        val json = Json.encodeToString(teamsStorage.values.map { SerializableTeam(it.id, it.points) })
+        File("teams.json").writeText(json)
+    }
+
+    fun loadTeams() {
+        val file = File("teams.json")
+        if (file.exists()) {
+            val teams = Json.decodeFromString<List<SerializableTeam>>(file.readText())
+            teamsStorage.putAll(teams.associate { it.id to Team(it.id, it.points) })
+        }
+    }
 }
+
+
